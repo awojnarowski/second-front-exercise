d0448c14baf8ab01018e7806ee727e88d04f3645
Adam <adamwojnarowki@gmail.com>
2021-04-22 21:12:19 -0400

    Feature: Added redditPostComponent hook, Componenet Queries reddit's API for 25 newest posts for the phrase 'AAPL' and dispalys them on the page

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/redditComponent.css';
+import RedditPostComponent from './redditPostComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test Text!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RedditPostComponent/>
   );
 }
 
---
+++ web/src/stockTickerComponent.js
@@ -0,0 +1,32 @@
+/**
+ * Container for stock ticker
+ * Component will query reddits API
+ */
+ import './css/redditComponent.css';
+ import React, { useState, useEffect } from 'react';
+ function RedditPostComponent(props) {
+ 
+     const [results, redditPosts] = useState([]);
+ 
+     useEffect(() => {
+         async function fetchData() {
+             const response = await fetch('http://www.reddit.com/search.json?q=AAPL+subreddit:stocks+sort=new');
+             const results = await response.json();
+             const postTitles = results.data.children.map(post => post.data.title);
+ 
+             redditPosts(postTitles);
+         }
+         fetchData();
+     }, [props.id]);
+ 
+     return (
+         <div className="reddit-post">
+             <header>
+                 <p>Reddit Posts!</p>
+                 {results.map(result => <div> {result} </div>)}
+             </header>
+         </div>
+     );
+ }
+ 
+ export default RedditPostComponent;
