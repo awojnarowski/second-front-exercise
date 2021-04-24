557d0b496f72e88421c764841126895850205037
Adam <adamwojnarowki@gmail.com>
2021-04-23 22:33:33 -0400

    FEATURE: RedditPostComponent now updates with the parent input tag.

--- web/src/redditPostComponent.js
+++ web/src/redditPostComponent.js
@@ -5,24 +5,24 @@
 import './css/redditComponent.css';
 import React, { useState, useEffect } from 'react';
 function RedditPostComponent(props) {
-
+    const { ticker } = props;
     const [results, redditPosts] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
-            const response = await fetch('http://www.reddit.com/search.json?q=AAPL+subreddit:stocks+sort=new');
+            const response = await fetch(`http://www.reddit.com/search.json?q=${ticker}+subreddit:stocks+sort=new`);
             const results = await response.json();
             const postTitles = results.data.children.map(post => post.data.title);
 
             redditPosts(postTitles);
         }
         fetchData();
-    }, [props.id]);
+    }, [ticker]);
 
     return (
         <div className="reddit-post">
             <header>
-                <p>Reddit Posts!</p>
+                <p>What's r/stocks saying about {ticker}?</p>
                 {results.map(result => <div> {result} </div>)}
             </header>
         </div>
